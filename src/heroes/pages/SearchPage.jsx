d0f71de9e150105e5f8c69fb5645e94610040c58
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2024-04-21 21:26:00 -0300

    necesito pulir el imput y sacar errores

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,27 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import queryString from "query-string";
+
+// import { HeroCard } from "../components";
+
 // import HeroCard from "../components";
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+
+  const { searchText, onInputChange } = useForm({
+    serchText: "",
+  });
+
+  const onSerchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().lenght <= 1) return;
+
+    navigate(`?q=${searchText}`);
+  };
+
   return (
     <>
       <h1>Search</h1>
@@ -9,13 +31,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={onSerchSubmit}>
             <input
               type="text"
               placeholder="Search a Hero"
               className="form-control"
-              name="seachText"
+              name="searchText"
               autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
             />
             <button className="btn btn-outline-primary mt-1">Search</button>
           </form>
@@ -27,7 +51,7 @@ export const SearchPage = () => {
 
           <div className="alert alert-primary"> Search a Hero</div>
           <div className="alert alert-danger">
-            No hero with <b>ABC</b>
+            No hero with <b>{q}</b>
           </div>
 
           {/* <HeroCard /> */}
